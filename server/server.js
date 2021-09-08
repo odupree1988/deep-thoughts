2d454e96d6b793d4bf1fe9ac13e711c7bde27b5a
odupree1988 <odupree1988@gmail.com>
2021-09-08 13:13:04 -0400

    feature/routes

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require( 'apollo-server-core');
+const http = require('http');
+const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+async function startApolloServer(typeDefs, resolvers) {
+  // Required logic for integrating with Express
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  // Same ApolloServer initialization as before, plus the drain plugin.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware ,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  // More required logic for integrating with Express
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/graphql'
+  });
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  // Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+  // Modified server startup
+  await new Promise(resolve => httpServer.listen({ port: 3001 }, resolve));
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers)
