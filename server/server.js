edc2ea87d248bdaf410db37ae9cf2ee8f3e8ff82
odupree1988 <odupree1988@gmail.com>
2021-09-08 15:54:14 -0400

    added routes to SPA

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
-const { ApolloServerPluginDrainHttpServer } = require( 'apollo-server-core');
-const http = require('http');
-const path = require('path');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const http = require("http");
+const path = require("path");
 
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,7 +19,7 @@ async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware ,
+    context: authMiddleware,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
@@ -27,25 +27,29 @@ async function startApolloServer(typeDefs, resolvers) {
   await server.start();
   server.applyMiddleware({
     app,
-    path: '/graphql'
+    path: "/graphql",
   });
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
   // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
+  // app.get("*", (req, res) => {
+  //   res.status(404).sendFile(path.join(__dirname, "./public/404.html"));
+  // });
 
   // Modified server startup
-  await new Promise(resolve => httpServer.listen({ port: 3001 }, resolve));
+  await new Promise((resolve) => httpServer.listen({ port: 3001 }, resolve));
   console.log(`API server running on port ${PORT}!`);
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 }
 
-startApolloServer(typeDefs, resolvers)
+startApolloServer(typeDefs, resolvers);
